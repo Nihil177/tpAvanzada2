80b614dc9bb3ad07d90452e468bc3409b368e532
Nihil177 <63241468+Nihil177@users.noreply.github.com>
2020-09-07 14:00:30 -0300

    Add files via upload

---
+++ procesoLogin.php
@@ -0,0 +1,52 @@
+ Ingredientes
+1lb de pasta para lasaña (1/2 kg)
+4 litros de agua
+1 cucharada de aceite
+1 cucharada de sal o de consomé de pollo en polvo
+
+Para el relleno:
+2 lbs de carne molida (1/2 kg)
+1 cebolla mediana picada
+2 dientes de ajo picados
+1 cucharada de aceite de oliva
+2 latas medianas de salsa para spaghetti
+½ taza de agua o vino blanco
+15 oz de queso ricotta o queso crema
+½ taza de queso parmesano rallado
+1 taza de queso para quesadilllas o mozarella
+1 cucharada de albahacar seca (basil) u orégano
+Sal y pimienta al gusto
+
+Calentamos el agua (4 litros) con una cucharada de aceite, podemos agregar sal o consomé de pollo. Cuando empiece a hervir, agregamos las láminas de pasta y mezclamos un poco solo para separarlas. El agua debe de cubrir la pasta.
+
+Dejamos cocer aproximadamente 7-8 minutos o hasta que la pasta este suave. (Mientras puedes ir picando los vegetales que vamos a utilizar para el relleno).
+
+Pasado este tiempo, escurrimos y separamos.
+
+Calentamos el horno a 375F o 190 C.
+
+Para el relleno.
+
+Calentamos un sartén, agregamos aceite de oliva y freímos la cebolla y el ajo, hasta que la cebolla quede acitronada (transparente).
+
+Incorporamos la carne molida y freímos.
+
+Agregamos la media taza de vino o agua, una lata de la salsa de spaghetti, el albahacar u orégano, sal, pimienta y bajamos el fuego, dejamos hervir. Retiramos del fuego.
+
+Aparte, engrasamos un refractario rectangular, agregamos la mitad de una lata de salsa de spaghetti para cubrir el fondo del molde.
+
+
+Acomodamos las láminas de lasaña, haciendo capas. Colocamos una capa de lasaña, una parte de la mezcla de carne, queso ricota queso parmesano. Repetimos hasta que terminemos con la pasta.
+
+Encima de la última capa, agregamos salsa de spaghetti.
+
+receta para preparar lasaña gratis
+Tapamos con papel aluminio y metemos al horno por treinta minutos.
+
+Sacamos del horno y cubrimos con queso quesadilla y agregamos cualquier queso que haya sobrado.
+
+Metemos al horno sin tapar por cinco minutos o hasta que gratine o derrita el queso de encima.
+
+Sacamos del horno y dejamos reposar por cinco minutos antes de servir, para que no se desbarate.
+
+Provecho!
